37ec1d0fba3f9c1b0d0bd342a8073e6572fc0d76
goofyww <akindofattitude@163.com>
2021-07-21 20:50:26 +0800

    拥有继承关系的 InheritableThreadLocal

--- src/main/java/cn/goofyww/concurrent/demo/threadpool/DefaultThreadPoolTest.java
+++ src/main/java/cn/goofyww/concurrent/demo/threadpool/DefaultThreadPoolTest.java
@@ -2,11 +2,16 @@ package cn.goofyww.concurrent.demo.threadpool;
 
 public class DefaultThreadPoolTest {
 
+    /** 拥有继承关系的 InheritableThreadLocal **/
+    private static final InheritableThreadLocal<String> tl = new InheritableThreadLocal<String>();
+
     public static void main(String[] args) {
 
+        tl.set("13001217708");
         DefaultThreadPool pool = new DefaultThreadPool();
         pool.execute(() -> {
-            System.out.println(Thread.currentThread().getName() + " : hello world");
+            System.out.println(Thread.currentThread().getName() + ":hello world ");
+            System.out.println(tl.get());
         });
 
     }
