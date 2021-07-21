c81e26f07d7b24fa49ff36b9827460ac07b2f6f0
goofyww <akindofattitude@163.com>
2021-07-21 12:15:03 +0800

    自定义的排斥锁v2

--- src/main/java/cn/goofyww/concurrent/lock/MutexLock.java
+++ src/main/java/cn/goofyww/concurrent/lock/MutexLock.java
@@ -1,4 +1,103 @@
 package cn.goofyww.concurrent.lock;
 
-public class MutexLock {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 自定义的排斥锁
+ */
+public class MutexLock implements Lock {
+
+
+    private static final class Sync extends AbstractQueuedSynchronizer {
+
+        /**
+         * 是否处于占用状态
+         *
+         * @return
+         */
+        @Override
+        protected boolean isHeldExclusively() {
+            return getExclusiveOwnerThread() == Thread.currentThread();
+        }
+
+        /**
+         * 当状态为0时，获取锁
+         *
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * 释放锁，将状态设置为0
+         *
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalArgumentException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        /**
+         * 返回一个Condition，每个condition都包含一个condition队列
+         *
+         * @return
+         */
+        public Condition newCondition() {
+            return new ConditionObject();
+        }
+
+    }
+
+    /**
+     * 将对使用者的所有操作代理至 Sync 上
+     **/
+    private final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(timeout));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
 }
